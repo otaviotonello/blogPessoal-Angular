8136d700dffd37f81643648d3ba97a53aaf8b364
otaviotonello <otaviotonello0102@gmail.com>
2022-01-24 11:08:57 -0300

    Criando tela de inicio

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserLogin } from '../model/UserLogin';
 import { User } from '../model/User';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,15 @@ export class AuthService {
   cadastrar(user: User): Observable<User>{
       return this.http.post<User>('https://blogdootaviotonello.herokuapp.com/usuarios/cadastrar', user)
     
+  }
+
+  logado() {
+    let ok: boolean = false
+
+    if(environment.token != '') {
+      ok = true
+    }
+
+    return ok
   }
 }
